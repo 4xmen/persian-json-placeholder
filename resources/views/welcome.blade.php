8155a25c493f4283696833645c781e367ac9d71d
freeman <a1gard@4xmen.ir>
2023-01-28 05:45:22 +0330

    fix welcome page title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Persian json placeholder for developer - xstack</title>
 
         <!-- Fonts -->
         <link href="https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css" rel="stylesheet" type="text/css" />
